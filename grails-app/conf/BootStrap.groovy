ab7b65ada419c224c9b58ca8d68dcb4e47ca4969
Trond Valen <trondvalen@gmail.com>
2012-11-17 21:30:12 +0100

    Lagt til innlogging

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,4 +1,7 @@
-import grails20test.Post
+import com.trondvalen.blogster.Post
+import com.trondvalen.blogster.Role
+import com.trondvalen.blogster.User
+import com.trondvalen.blogster.UserRole;
 
 class BootStrap {
 
@@ -6,6 +9,14 @@ class BootStrap {
 		if (!Post.count()) {
             new Post(user: "Mike", content : "Hi all").save(failOnError: true)            
         }
+		def adminRole = new Role(authority: "ROLE_ADMIN").save()
+		def memberRole = new Role(authority: 'ROLE_MEMBER').save()
+		
+		def member = new User(username: "member", password: "member", enabled: true)
+		def admin = new User(username: "admin", password: "admin", enabled: true)
+		
+		UserRole.create(member, memberRole)
+		UserRole.create(admin, adminRole)
 
     }
     def destroy = {
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -91,3 +91,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'com.trondvalen.blogster.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'com.trondvalen.blogster.UserRole'
+grails.plugins.springsecurity.authority.className = 'com.trondvalen.blogster.Role'
